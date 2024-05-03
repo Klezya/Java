29dc0c33a7c301a38e9fa71661513ab33222798f
Klezya <benjamin.nunez.beat@gmail.com>
2024-05-03 16:25:33 -0400

    Initial commit

---
+++ src/main/MainPackage/main.java
@@ -0,0 +1,15 @@
+package src.main.MainPackage;
+import src.main.ventana;
+
+
+public class main {
+    
+    public static void main(String[] args) {
+        
+        
+        ventana ventana = new ventana();
+
+    }
+
+
+}
